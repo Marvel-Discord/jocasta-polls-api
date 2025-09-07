a3c300ac8129b130460888f53c9417c1c6c337c8
Rocked03 <rockedohthree@gmail.com>
2025-09-07 14:30:13 +1200

    feat: listener

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+prisma.$connect().then(() => {
+  return prisma.$executeRaw`SET app.current_platform = 'web'`;
+});
